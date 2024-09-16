816c1959ae64fd16ca94a96325e17d95dfa371ca
Wellygnton Chaves de Matos <wellygnton.matos@compasso.com.br>
2024-09-16 10:44:21 -0300

    update app.py

--- api/app.py
+++ api/app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+
+instrumentator = Instrumentator()
+instrumentator.instrument(app).expose(app, endpoint="/metrics")
 
 @app.get('/')
 def read_root():
